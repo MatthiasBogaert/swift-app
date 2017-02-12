fd983343679c21c615317356f1381e4b061b6772
Matthias Bogaert <hello@matthiasbogaert.be>
2017-02-12 23:49:41 +0100

    my first commit

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -3,13 +3,30 @@
 //  Swift App
 //
 //  Created by De Winter Laura on 12/02/17.
-//  Copyright © 2017 Comflow. All rights reserved.
+//  Copyright © 2017 Comflow. All rights reserved..
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Thelabel: UILabel!
+    
+    var tapCount = 0
+    
+    
+    @IBAction func Pushmetapped(_ sender: Any) {
+        
+        Thelabel.text = "Honey"
+        
+        tapCount = tapCount + 1
+        
+        if tapCount >= 20 {
+            Thelabel.text = "20x geklikt"
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
