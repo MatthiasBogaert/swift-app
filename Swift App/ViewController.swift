9b3ef293f1e95fc385a70e40d068938ce5adf845
De Winter Laura <dewinterlaura@macbook-pro-van-de.local>
2017-02-12 23:31:23 +0100

    Initial Commit

---
+++ Swift App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift App
+//
+//  Created by De Winter Laura on 12/02/17.
+//  Copyright © 2017 Comflow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
